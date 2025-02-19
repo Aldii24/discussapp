c153e0e5c9a6b7e2fae5933bebffea7007109c64
Aldii24 <aldiirawan748@gmail.com>
2025-02-19 16:31:23 +0700

    "production"

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cookieParser())
 
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://discussapp-mu.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
