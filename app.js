f3ca2d7017c9f46a789c48803672b59ce74afebf
Aldii24 <aldiirawan748@gmail.com>
2025-02-19 16:38:16 +0700

    "production"

--- app.js
+++ app.js
@@ -16,12 +16,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
 
 
-app.use(cors({
-    origin: ["http://localhost:3000", "https://discussapp-mu.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-}));
+// app.use(cors({
+//     origin: ["http://localhost:3000", "https://discussapp-mu.vercel.app"],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     credentials: true,
+// }));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+
+    next();
+});
 
 app.use(errorMiddleware)
 
